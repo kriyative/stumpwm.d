a29b9b47bd31449c34e66100d19eeb03aa83a51c
Ram Krishnan <kriyative@gmail.com>
2017-06-30 00:39:17 -0700

    Adds a naive Emacs winner mode clone

--- init.lisp
+++ init.lisp
@@ -2,6 +2,69 @@
 
 (in-package :stumpwm)
 
+(defvar *group-undo-stack* nil)
+(defvar *group-redo-stack* nil)
+(defvar *group-stack-max-depth* 10)
+
+(defun reset-group-stacks ()
+  (setq *group-undo-stack* nil
+        *group-redo-stack* nil))
+
+;; (reset-group-stacks)
+;; (length *group-undo-stack*)
+;; (length *group-redo-stack*)
+
+(defmacro push-max-stack (stack val max-depth)
+  `(setq ,stack
+         (cons ,val
+               (if (<= ,max-depth (length ,stack))
+                   (subseq ,stack 0 (1- ,max-depth))
+                   ,stack))))
+
+(defun push-group ()
+  "Save current group placement"
+  (push-max-stack *group-undo-stack*
+                  (dump-group (current-group))
+                  *group-stack-max-depth*))
+
+(defun group-stack-pre-command-hook (command)
+  (when (member command '(hsplit vsplit only) :test #'eq)
+    (push-group)))
+
+(add-hook *pre-command-hook* 'group-stack-pre-command-hook)
+
+(defun pop-group ()
+  "Restore last group placement"
+  (let ((gdump (pop *group-undo-stack*)))
+    (if gdump
+        (progn
+          (push-max-stack *group-redo-stack*
+                          (dump-group (current-group))
+                          *group-stack-max-depth*)
+          (restore-group (current-group) gdump))
+        (message "Can't go back anymore"))))
+
+(defcommand frame-undo () ()
+  "Undo frame changes"
+  (pop-group))
+
+(define-key *root-map* (kbd "C-b") "frame-undo")
+
+(defun redo-group ()
+  "Redo last undone group change"
+  (let ((gdump (pop *group-redo-stack*)))
+    (if gdump
+        (progn
+          (push-group)
+          (restore-group (current-group) gdump))
+        (message "Can't go forward anymore"))))
+
+(defcommand frame-redo () ()
+  "Redo recently undone frame changes"
+  (redo-group))
+
+(define-key *root-map* (kbd "C-f") "frame-redo")
+
 (load-module "amixer")
 (in-package :amixer)
 (defvolcontrol amixer-Master-10- "Master" "10%-")
