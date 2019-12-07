8f549492772a8deb024d5a031b2da29defc15900
Ram Krishnan <kriyative@gmail.com>
2019-12-07 13:25:02 -0800

    Refactors mail mode-line status reporting

--- init.lisp
+++ init.lisp
@@ -107,7 +107,46 @@
 
 (load-module "battery-portable")
 (in-package :battery-portable)
-(export '(sysfs-int-field-or-nil))
+
+(defun fmt-power-source (ml)
+  (declare (ignore ml))
+  (if (= 1
+         (sysfs-int-field-or-nil
+          "/sys/class/power_supply/AC/"
+          "online"))
+      "AC"
+      "BAT"))
+
+(defun fmt-bat-alt (ml)
+  (if (equal "BAT" (fmt-power-source ml))
+      (fmt-bat ml)
+      (flet ((battery-levels (battery)
+               (let ((path (path-of battery)))
+                 (if (string= (sysfs-field path "present")
+                              "0")
+                     0
+                     (let* ((curr (or (sysfs-int-field-or-nil path "energy_now")
+                                      ;; energy_* seems not to be there on
+                                      ;; some boxes. Strange...
+                                      (sysfs-int-field-or-nil path "charge_now")
+                                      (return-from fmt-bat-alt :unknown)))
+                            (full (or (sysfs-int-field-or-nil path "energy_full")
+                                      (sysfs-int-field-or-nil path "charge_full")
+                                      (return-from fmt-bat-alt :unknown)))
+                            (percent (* 100 (/ curr full))))
+                       (round percent))))))
+        (let ((pcts (mapcar #'battery-levels
+                            (all-batteries
+                             (or (preferred-battery-method)
+                                 (return-from fmt-bat-alt
+                                   "(not implemented)"))))))
+          (format nil
+                  "~D%"
+                  (round
+                   (/ (reduce '+ pcts :initial-value 0)
+                      (length pcts))))))))
+
+(add-screen-mode-line-formatter #\B #'fmt-bat-alt)
 
 (in-package :stumpwm)
 (ql:quickload "clx-truetype")
@@ -448,42 +487,60 @@
     (apply #'sb-ext:run-program prog args :output (if output output t) :search t
            :error (if error error t) :wait wait :environment env opts)))
 
-(defun fmt-mail-biff (mb)
-  (declare (ignore mb))
+(defvar *fmt-mail-biff-unread* nil)
+
+(defun fmt-mail-biff-poll-unread ()
   (let* ((out (ignore-errors
                 (let ((err (make-string-output-stream)))
                   (with-output-to-string (s)
                     (run-prog *mu-cmd*
                               :args '("find"
                                       "flag:unread"
-                                      "-f" "m,f,s"
+                                      "-f" "m	f	s"
                                       "-u")
                               :error err
                               :output s
                               :wait t)))))
          (msgs (unless (string-equal "" out)
-                 (split-string out)))
-         (msgs1 (remove-if (lambda (msg)
-                             (cl-ppcre:scan "All Mail" msg))
-                           msgs))
-         (cnt (length msgs1)))
+                 (mapcar (lambda (msg)
+                           (split-string msg "	"))
+                         (split-string out)))))
+    (setq *fmt-mail-biff-unread*
+          (remove-if (lambda (msg)
+                       (cl-ppcre:scan "All Mail" (first msg)))
+                     msgs))
+    *fmt-mail-biff-unread*))
+
+;; (fmt-mail-biff-poll-unread)
+
+(defun fmt-mail-biff (mb)
+  (declare (ignore mb))
+  (let* ((msgs (fmt-mail-biff-poll-unread))
+         (cnt (length msgs)))
     (format nil
             (if (positive-integer-p cnt)
                 "^B^3*~A:~D^*^b"
                 "~A:~D")
             "✉"
-            cnt)))
+            (length msgs))))
 
 ;; (fmt-mail-biff 0)
 
-(defun fmt-power-source (ml)
-  (declare (ignore ml))
-  (if (= 1
-         (battery-portable:sysfs-int-field-or-nil
-          "/sys/class/power_supply/AC/"
-          "online"))
-      "AC"
-    "BAT"))
+(defcommand rk-show-biff-unread () ()
+  "Show list of unread messages"
+  (if (zerop (length *fmt-mail-biff-unread*))
+      (message "No unread messages")
+      (select-from-menu (current-screen)
+                        (mapcar (lambda (msg)
+                                  (destructuring-bind (mbox sender subj)
+                                      msg
+                                    (format nil "~10a ~20,a ~a"
+                                            (scrunch mbox :maxlen 10)
+                                            (scrunch sender :maxlen 25)
+                                            (scrunch subj :maxlen 35))))
+                                *fmt-mail-biff-unread*)
+                        nil))
+  nil)
 
 (defvar *mode-line-brightness* 0.25)
 
