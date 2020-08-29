65cafff6d1657b10a7cec86b0cb551a5797143b8
Ram Krishnan <kriyative@gmail.com>
2020-08-29 00:23:47 -0700

    Uses local clx-truetype, no longer in quicklisp

--- fonts.lisp
+++ fonts.lisp
@@ -1,6 +1,7 @@
 (in-package :stumpwm)
 
-(ql:quickload "clx-truetype")
+;; (ql:quickload "archives/clx-truetype")
+(asdf:load-system "clx-truetype")
 (load-module "ttf-fonts")
 
 (in-package :stumpwm)
@@ -9,10 +10,9 @@
   (xft:cache-fonts)
   ;; (clx-truetype:get-font-families)
   ;; (clx-truetype:get-font-subfamilies "DejaVu Sans Mono")
-  ;; (clx-truetype:get-font-subfamilies "Noto Sans")
-  (let ((fonts '((:family "DejaVu Sans Mono" :subfamily "Book" :size 11)
-                 #+nil (:family "Noto Sans Devanagari"
-                        :subfamily "Regular" :size 12))))
+  ;; (clx-truetype:get-font-subfamilies "Noto Mono")
+  (let ((fonts '((:family "DejaVu Sans Mono" :subfamily "Book" :size 13)
+                 (:family "Noto Mono" :subfamily "Regular" :size 13))))
     (set-font (mapcar (lambda (font)
                         (apply 'make-instance 'xft:font font))
                       fonts))))
